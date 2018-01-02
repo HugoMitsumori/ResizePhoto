f2e2c4f09e3ebdc62338fe6a195b7095d450dbf3
HugoMitsumori <hugo.mitsumori@hotmail.com>
2018-01-02 03:34:29 -0200

    adding photos to db

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -8,24 +8,26 @@ module PhotosHelper
   }
 
 
+  # gets photos from URL and add its resized urls to database
   def fetch_photos
     response = RestClient.get(FETCH_URL)
     photos = JSON.parse(response)
 
     photos['images'].each do |photo|
       name = photo['url'].split('/').last
-      
+
       photo_file = download photo['url']
 
       resized = resize_photo photo_file.path, name
       resized['name'] = name.split('.').first
       resized['original'] = photo['url']
 
-      # Photo.new 
+      Photo.create(resized)
       photo_file.close(true)
     end
   end
 
+  # downloads content from url and returns it as a temp file
   def download (url)
     content = open(url).read
     file = Tempfile.new("temp", :encoding => 'ascii-8bit')
@@ -33,6 +35,7 @@ module PhotosHelper
     return file
   end
 
+  # generate all sizes for given photo
   def resize_photo (path, name)
     resized = {}
     SIZES.each do |size, dimensions|
