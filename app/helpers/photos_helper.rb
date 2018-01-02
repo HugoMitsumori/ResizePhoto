b69a000e1feeaba6fe0f05dd7f420afe56aff722
HugoMitsumori <hugo.mitsumori@hotmail.com>
2018-01-02 02:45:24 -0200

    downloads photos

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -8,19 +8,25 @@ module PhotosHelper
 
 		photos['images'].each do |photo|
 			photo_file = download photo['url']
-			resized = resize photo_file
-			resized['original'] = photo_file
+
+			resized = resize_photo photo_file.path
+			#resized['original'] = photo_file
 
 			# Photo.new 
+			photo_file.close(true)
 		end
-
-		byebug
 	end
 
 	def download (url)
-
+		content = open(url).read
+		file = Tempfile.new("temp", :encoding => 'ascii-8bit')
+		file << content
+		return file
 	end
 
-	def resize_photo
+	def resize_photo (path)
+		resized = {}
+
+		return resized
 	end
 end
